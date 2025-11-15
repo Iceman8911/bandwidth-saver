49c26321e29878572bff5a80c7a680dee72b0fc3
Iceman8911 <wuchiJSS3@gmail.com>
2025-11-15 22:00:06 +0100

    fix: use proper base4 return type

--- models/image-optimization.ts
+++ models/image-optimization.ts
@@ -1,5 +1,6 @@
-/** If the request did not succeed, return `null` instead of the failed response */
+import type { Base64Schema } from "./shared";
+
 export type ImageCompressionAdapter = (
 	originalUrl: string | URL,
 	quality?: number,
-) => Promise<Response | null>;
+) => Promise<Base64Schema | null>;
