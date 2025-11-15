81c1b2619c3e072edbb7db889f544ca42d852881
Iceman8911 <wuchiJSS3@gmail.com>
2025-11-15 15:45:41 +0100

    wip: add basic image compressor endpoints

---
+++ shared/constants.ts
@@ -0,0 +1,5 @@
+export enum ImageCompressorEndpoint {
+	WSRV_NL = "https://wsrv.nl/",
+	ALPACA_CDN = "https://spitting.alpacacdn.com/",
+}
+// TODO: Add bandwidth hero service and maybe a custom selfhost one
