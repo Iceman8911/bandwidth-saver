a9f94140c8444e03ba0c48f3a7a2c0ae2c79d528
Iceman8911 <wuchiJSS3@gmail.com>
2025-11-15 22:30:26 +0100

    fix: use fetch with timeout

--- utils/image-optimization/adapter.ts
+++ utils/image-optimization/adapter.ts
@@ -1,6 +1,9 @@
 import type { ImageCompressionAdapter } from "@/models/image-optimization";
 import { ImageCompressorEndpoint } from "@/shared/constants";
 import { convertBlobToBase64 } from "../blob";
+import { fetchWithTimeout } from "../fetch";
+
+const REQUEST_TIMEOUT = 3000;
 
 export const imageCompressionAdapterWsrvNl: ImageCompressionAdapter = async (
 	url,
@@ -12,7 +15,7 @@ export const imageCompressionAdapterWsrvNl: ImageCompressionAdapter = async (
 
 	const newUrl = `${ImageCompressorEndpoint.WSRV_NL}?${urlParams}`;
 
-	const response = await fetch(newUrl);
+	const response = await fetchWithTimeout(REQUEST_TIMEOUT, newUrl);
 
 	if (!response.ok) return null;
 
@@ -27,7 +30,7 @@ export const imageCompressionAdapterAlpacaCdn: ImageCompressionAdapter = async (
 
 	const newUrl = `${ImageCompressorEndpoint.ALPACA_CDN}?${urlParams}`;
 
-	const response = await fetch(newUrl);
+	const response = await fetchWithTimeout(REQUEST_TIMEOUT, newUrl);
 
 	if (!response.ok) return null;
 
