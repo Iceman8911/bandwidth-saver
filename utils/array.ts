149047e22e9a083cb0c962861290a4183ed674bf
Iceman8911 <wuchiJSS3@gmail.com>
2025-11-15 23:01:07 +0100

    feat: add util for getting a random array element

---
+++ utils/array.ts
@@ -0,0 +1,11 @@
+export function getRandomElementInArray(
+	array: ReadonlyArray<unknown> & { length: 0 },
+): undefined;
+export function getRandomElementInArray<TArrayElement>(
+	array: ReadonlyArray<TArrayElement>,
+): TArrayElement;
+export function getRandomElementInArray<TArrayElement>(
+	array: ReadonlyArray<TArrayElement>,
+): TArrayElement | undefined {
+	return array[Math.floor(Math.random() * array.length)];
+}
