0bf2009e7bc234478a784bb8dceab05eccfd1c62
Iceman8911 <wuchiJSS3@gmail.com>
2025-11-15 14:41:44 +0100

    chore: add enum optimiser

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,8 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
+import InlineEnum from "unplugin-inline-enum/vite";
 import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
 	modules: ["@wxt-dev/module-solid"],
-	vite: () => ({ plugins: [tailwindcss()] }),
+	vite: () => ({ plugins: [tailwindcss(), InlineEnum()] }),
 });
