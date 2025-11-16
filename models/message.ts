44d3c4bfad8f77b286b50378c1fea23535a3db0c
Iceman8911 <wuchiJSS3@gmail.com>
2025-11-16 02:00:05 +0100

    feat: add basic runtime message schema

---
+++ models/message.ts
@@ -0,0 +1,17 @@
+import * as v from "valibot";
+import { MessageType } from "@/shared/constants";
+import { UrlSchema } from "./shared";
+
+const ValidateCompressedUrlMessageSchema = v.pipe(
+	v.object({
+		type: v.literal(MessageType.VALIDATE_COMPRESSION_URL),
+		/** The url to the compressed remote  */
+		url: UrlSchema,
+	}),
+	v.readonly(),
+);
+
+export const RuntimeMessageSchema = v.variant("type", [
+	ValidateCompressedUrlMessageSchema,
+]);
+export type RuntimeMessageSchema = v.InferOutput<typeof RuntimeMessageSchema>;
