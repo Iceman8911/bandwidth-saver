9a72d0ad705c7c37dda6d0af00d36909a7257258
Iceman8911 <wuchiJSS3@gmail.com>
2025-11-17 20:01:51 +0100

    feat: add simple redirect to compression service

--- packages/extension/entrypoints/background/index.ts
+++ packages/extension/entrypoints/background/index.ts
@@ -1,9 +1,14 @@
-import { checkIfUrlReturnsValidResponse } from "@bandwidth-saver/shared";
+import {
+	checkIfUrlReturnsValidResponse,
+	ImageCompressorEndpoint,
+} from "@bandwidth-saver/shared";
 import * as v from "valibot";
 import { RuntimeMessageSchema } from "@/models/message";
-import { MessageType } from "@/shared/constants";
+import { CompressionMode, MessageType } from "@/shared/constants";
+import { compressionSettingsStorageItem } from "@/shared/storage";
 
 const ADD_SAVE_DATA_HEADER_RULE_ID = 1;
+const REDIRECT_TO_SIMPLE_COMPRESSION_PROXY_RULE_ID = 2;
 
 function appendSaveDataToAllRequests() {
 	browser.declarativeNetRequest.updateDynamicRules({
@@ -32,6 +37,70 @@ function appendSaveDataToAllRequests() {
 	});
 }
 
+async function redirectToFirstCompressorEndpointIfPossible() {
+	const { enabled, mode, quality, format } =
+		await compressionSettingsStorageItem.getValue();
+
+	if (!enabled || mode !== CompressionMode.SIMPLE) {
+		await browser.declarativeNetRequest.updateDynamicRules({
+			addRules: [],
+			removeRuleIds: [REDIRECT_TO_SIMPLE_COMPRESSION_PROXY_RULE_ID],
+		});
+		return;
+	}
+
+	const host = ImageCompressorEndpoint.DEFAULT;
+	const hostWithoutProtocol = host.replace(/^https?:\/\//, "");
+	const scheme = host.startsWith("http://") ? "http" : "https";
+
+	await browser.declarativeNetRequest.updateDynamicRules({
+		addRules: [
+			{
+				action: {
+					redirect: {
+						transform: {
+							host: hostWithoutProtocol,
+							path: "/",
+							queryTransform: {
+								addOrReplaceParams: [
+									{
+										key: "url",
+										value: "{url}",
+									},
+									{
+										key: "q",
+										value: `${quality}`,
+									},
+									{
+										key: "output",
+										value: format,
+									},
+								],
+							},
+							scheme,
+						},
+					},
+					type: "redirect",
+				},
+				condition: {
+					excludedInitiatorDomains: [hostWithoutProtocol],
+					excludedRequestDomains: [hostWithoutProtocol],
+					resourceTypes: ["image"],
+					urlFilter: "*",
+				},
+				id: REDIRECT_TO_SIMPLE_COMPRESSION_PROXY_RULE_ID,
+			},
+		],
+		removeRuleIds: [REDIRECT_TO_SIMPLE_COMPRESSION_PROXY_RULE_ID],
+	});
+}
+
+function watchCompressionSettingsChanges() {
+	compressionSettingsStorageItem.watch(() => {
+		redirectToFirstCompressorEndpointIfPossible();
+	});
+}
+
 function checkIfCompressionUrlFromContentScriptIsValid() {
 	browser.runtime.onMessage.addListener((message, _, sendResponse) => {
 		const parsedMessage = v.parse(RuntimeMessageSchema, message);
@@ -50,6 +119,9 @@ function checkIfCompressionUrlFromContentScriptIsValid() {
 
 export default defineBackground(() => {
 	appendSaveDataToAllRequests();
+
+	redirectToFirstCompressorEndpointIfPossible();
+	watchCompressionSettingsChanges();
 
 	checkIfCompressionUrlFromContentScriptIsValid();
 });
