729af60d4ec3e930082941aa5123115a40425260
Iceman8911 <wuchiJSS3@gmail.com>
2025-11-22 00:15:27 +0100

    chore: fix install

--- packages/extension/entrypoints/popup/App.tsx
+++ packages/extension/entrypoints/popup/App.tsx
@@ -1,7 +1,24 @@
 // import { Tooltip } from "@/components/tooltip"
+
 import { getRandomUUID } from "@bandwidth-saver/shared";
 import { createSignal } from "solid-js";
 import * as v from "valibot";
+import {
+	blockSettingsStorageItem,
+	compressionSettingsStorageItem,
+	globalSettingsStorageItem,
+	proxySettingsStorageItem,
+	siteScopedBlockSettingsStorageItem,
+	siteScopedCompressionSettingsStorageItem,
+	siteScopedGlobalSettingsStorageItem,
+	siteScopedProxySettingsStorageItem,
+	siteStatisticsStorageItem,
+	statisticsStorageItem,
+} from "@/shared/storage";
+import { convertStorageItemToReadonlySignal } from "@/utils/reactivity";
+import { getActiveTabOrigin } from "@/utils/tabs";
+
+const activeTabUrl = await getActiveTabOrigin();
 
 const PopupScope = v.picklist(["domain", "global"]);
 type PopupScope = v.InferOutput<typeof PopupScope>;
@@ -18,7 +35,7 @@ function Header() {
 	};
 
 	return (
-		<div class="flex gap-4">
+		<header class="flex gap-4">
 			<h2 class="text-center font-semibold text-base text-primary">
 				Bandwidth Saver
 			</h2>
@@ -38,43 +55,121 @@ function Header() {
 					</option>
 				</select>
 			</label>
-		</div>
+		</header>
 	);
 }
 
 function Statistics() {
+	const _resolvedGlobalStatistics = convertStorageItemToReadonlySignal(
+		statisticsStorageItem,
+	);
+	const _resolvedSiteStatistics = convertStorageItemToReadonlySignal(
+		siteStatisticsStorageItem,
+	);
+
+	const statistics = createMemo(() =>
+		scope() === "global"
+			? _resolvedGlobalStatistics()
+			: activeTabUrl
+				? _resolvedSiteStatistics()?.[activeTabUrl]
+				: null,
+	);
+
 	return (
 		<div class="grid grid-cols-2 grid-rows-2 gap-4 text-info">
-			<div>Requests Processed: {100}</div>
+			<Suspense>
+				<div>Requests Processed: {statistics()?.requestsCompressed ?? 0}</div>
 
-			<div>Requests Blocked: {10}</div>
+				<div>Requests Blocked: {statistics()?.requestsBlocked ?? 0}</div>
 
-			<div>Data Consumed: {100} MB</div>
+				<div>
+					Data Consumed:{" "}
+					{(statistics()?.requestsCompressed ?? 0) / (1024 * 1024)} MB
+				</div>
 
-			<div>Data Saved: {45}% </div>
+				<div>Data Saved: {45}% </div>
+			</Suspense>
 		</div>
 	);
 }
 
 function DisableToggles() {
+	const globalSettings = convertStorageItemToReadonlySignal(
+		globalSettingsStorageItem,
+	);
+
+	const siteScopeGlobalSettings = convertStorageItemToReadonlySignal(
+		siteScopedGlobalSettingsStorageItem,
+	);
+
+	const isEnabledGlobally = () => globalSettings()?.enabled;
+
+	const isEnabledForSite = () =>
+		!!activeTabUrl && siteScopeGlobalSettings()?.[activeTabUrl]?.enabled;
+
+	const handleGlobalToggle = () =>
+		globalSettingsStorageItem.setValue({
+			...(globalSettings() ?? {}),
+			enabled: !isEnabledGlobally(),
+		});
+
+	const handleSiteToggle = () => {
+		if (!activeTabUrl) return;
+
+		const settings = siteScopeGlobalSettings();
+
+		siteScopedGlobalSettingsStorageItem.setValue({
+			...settings,
+			[activeTabUrl]: {
+				...settings?.[activeTabUrl],
+				enabled: !!isEnabledForSite(),
+			},
+		});
+	};
+
 	return (
 		<div class="flex justify-around gap-4">
 			<label class="space-x-2">
 				<span class="label">Enabled globally</span>
 
-				<input checked class="toggle toggle-primary" type="checkbox" />
+				<input
+					checked={isEnabledGlobally()}
+					class="toggle toggle-primary"
+					onClick={handleGlobalToggle}
+					type="checkbox"
+				/>
 			</label>
 
 			<label class="space-x-2">
 				<span class="label">Enabled on this site</span>
 
-				<input checked class="toggle toggle-primary" type="checkbox" />
+				<input
+					checked={isEnabledForSite()}
+					class="toggle toggle-secondary"
+					onClick={handleSiteToggle}
+					type="checkbox"
+				/>
 			</label>
 		</div>
 	);
 }
 
 function BlockSettingSummary(props: { name: string }) {
+	const _resolvedGlobalBlockSettings = convertStorageItemToReadonlySignal(
+		blockSettingsStorageItem,
+	);
+	const _resolvedSiteBlockSettings = convertStorageItemToReadonlySignal(
+		siteScopedBlockSettingsStorageItem,
+	);
+
+	const _blockSettings = createMemo(() =>
+		scope() === "global"
+			? _resolvedGlobalBlockSettings()
+			: activeTabUrl
+				? _resolvedSiteBlockSettings()?.[activeTabUrl]
+				: null,
+	);
+
 	return (
 		<div class="collapse-arrow join-item collapse border border-base-300">
 			<input name={props.name} type="radio" />
@@ -85,6 +180,21 @@ function BlockSettingSummary(props: { name: string }) {
 }
 
 function CompressionSettingSummary(props: { name: string }) {
+	const _resolvedGlobalCompressionSettings = convertStorageItemToReadonlySignal(
+		compressionSettingsStorageItem,
+	);
+	const _resolvedSiteCompressionSettings = convertStorageItemToReadonlySignal(
+		siteScopedCompressionSettingsStorageItem,
+	);
+
+	const _compressionSettings = createMemo(() =>
+		scope() === "global"
+			? _resolvedGlobalCompressionSettings()
+			: activeTabUrl
+				? _resolvedSiteCompressionSettings()?.[activeTabUrl]
+				: null,
+	);
+
 	return (
 		<div class="collapse-arrow join-item collapse border border-base-300">
 			<input name={props.name} type="radio" />
@@ -95,11 +205,55 @@ function CompressionSettingSummary(props: { name: string }) {
 }
 
 function ProxySettingSummary(props: { name: string }) {
+	const _resolvedGlobalProxySettings = convertStorageItemToReadonlySignal(
+		proxySettingsStorageItem,
+	);
+	const _resolvedSiteProxySettings = convertStorageItemToReadonlySignal(
+		siteScopedProxySettingsStorageItem,
+	);
+
+	const _proxySettings = createMemo(() =>
+		scope() === "global"
+			? _resolvedGlobalProxySettings()
+			: activeTabUrl
+				? _resolvedSiteProxySettings()?.[activeTabUrl]
+				: null,
+	);
+
 	return (
 		<div class="collapse-arrow join-item collapse border border-base-300">
 			<input name={props.name} type="radio" />
 			<div class="collapse-title font-semibold">Proxy Settings</div>
-			<div class="collapse-content text-sm">TODO</div>
+			<div class="collapse-content text-sm">
+				<form class="grid grid-cols-2 gap-4">
+					{/* Host */}
+					<fieldset class="fieldset">
+						<legend class="fieldset-legend">Host</legend>
+						<input class="input" placeholder="localhost" type="text" />
+						{/*<p class="label">You can edit page title later on from settings</p>*/}
+					</fieldset>
+
+					{/* Port */}
+					<fieldset class="fieldset">
+						<legend class="fieldset-legend">Port</legend>
+						<input
+							class="input"
+							max={65536}
+							min={0}
+							placeholder="localhost"
+							type="number"
+						/>
+						{/*<p class="label">You can edit page title later on from settings</p>*/}
+					</fieldset>
+
+					{/* Enabled */}
+					<fieldset class="fieldset">
+						<legend class="fieldset-legend">Enabled</legend>
+						<input class="input" placeholder="localhost" type="text" />
+						{/*<p class="label">You can edit page title later on from settings</p>*/}
+					</fieldset>
+				</form>
+			</div>
 		</div>
 	);
 }
@@ -119,7 +273,7 @@ function SettingSummaries() {
 }
 
 function Footer() {
-	return "";
+	return <footer>Made with love :3</footer>;
 }
 
 export default function App() {
