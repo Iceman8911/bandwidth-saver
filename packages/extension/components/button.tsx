a00036a44f24aa5d6a42b38a54b61e1e9147be7b
Iceman8911 <wuchiJSS3@gmail.com>
2025-11-22 21:10:15 +0100

    feat: add button component

---
+++ packages/extension/components/button.tsx
@@ -0,0 +1,13 @@
+import type { ComponentProps } from "solid-js";
+
+export function BaseButton(props: ComponentProps<"button">) {
+	return (
+		<button
+			{...props}
+			class={`btn ${props.class || ""}`}
+			type={props.type ?? "button"}
+		>
+			{props.children}
+		</button>
+	);
+}
