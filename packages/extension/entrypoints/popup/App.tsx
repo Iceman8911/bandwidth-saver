3fd1745ef03eb9d71c2675970552e36dbf3f2eda
Iceman8911 <wuchiJSS3@gmail.com>
2025-11-23 17:05:49 +0100

    wip: work on the popup

--- packages/extension/entrypoints/popup/App.tsx
+++ packages/extension/entrypoints/popup/App.tsx
@@ -1,6 +1,6 @@
 // import { Tooltip } from "@/components/tooltip"
 
-import { getRandomUUID } from "@bandwidth-saver/shared";
+import { capitalizeString, getRandomUUID } from "@bandwidth-saver/shared";
 import { createSignal } from "solid-js";
 import * as v from "valibot";
 import { BaseButton } from "@/components/button";
@@ -165,6 +165,20 @@ function DisableToggles() {
 }
 
 function BlockSettingSummary(props: { name: string }) {
+	const BLOCK_SETTING_SCHEMA = STORAGE_SCHEMA[StorageKey.SETTINGS_BLOCK].item;
+	type BLOCK_SETTING_SCHEMA = v.InferOutput<typeof BLOCK_SETTING_SCHEMA>;
+
+	const DEFAULT_BLOCK_SETTING = v.parse(BLOCK_SETTING_SCHEMA, {
+		enabled: false,
+		fileType: "image",
+		minSize: 80,
+		type: "type",
+	} as const satisfies BLOCK_SETTING_SCHEMA);
+
+	const blockTypes = ["ext", "mime", "type", "url"] as const satisfies Array<
+		BLOCK_SETTING_SCHEMA["type"]
+	>;
+
 	const _resolvedGlobalBlockSettings = convertStorageItemToReadonlySignal(
 		blockSettingsStorageItem,
 	);
@@ -172,13 +186,57 @@ function BlockSettingSummary(props: { name: string }) {
 		siteScopedBlockSettingsStorageItem,
 	);
 
-	const _blockSettings = createMemo(() =>
+	const blockSettings = createMemo(() =>
 		scope() === "global"
-			? _resolvedGlobalBlockSettings()
+			? (_resolvedGlobalBlockSettings() ?? [])
 			: activeTabUrl
-				? _resolvedSiteBlockSettings()?.[activeTabUrl]
-				: null,
+				? (_resolvedSiteBlockSettings()?.[activeTabUrl] ?? [])
+				: [],
 	);
+
+	const handleAddNewBlockSetting = () => {
+		const newSettings = [...blockSettings(), DEFAULT_BLOCK_SETTING];
+
+		if (scope() === "global") {
+			blockSettingsStorageItem.setValue(newSettings);
+		} else if (activeTabUrl) {
+			siteScopedBlockSettingsStorageItem.setValue({
+				..._resolvedSiteBlockSettings(),
+				[activeTabUrl]: newSettings,
+			});
+		}
+	};
+
+	const handleUpdateBlockSetting = (
+		idxToUpdate: number,
+		value: BLOCK_SETTING_SCHEMA,
+	) => {
+		const newSettings = blockSettings().map((setting, idx) =>
+			idx === idxToUpdate ? v.parse(BLOCK_SETTING_SCHEMA, value) : setting,
+		);
+
+		if (scope() === "global") {
+			blockSettingsStorageItem.setValue(newSettings);
+		} else if (activeTabUrl) {
+			siteScopedBlockSettingsStorageItem.setValue({
+				..._resolvedSiteBlockSettings(),
+				[activeTabUrl]: newSettings,
+			});
+		}
+	};
+
+	const handleRemoveBlockSetting = (idxToRemove: number) => {
+		const newSettings = blockSettings().toSpliced(idxToRemove);
+
+		if (scope() === "global") {
+			blockSettingsStorageItem.setValue(newSettings);
+		} else if (activeTabUrl) {
+			siteScopedBlockSettingsStorageItem.setValue({
+				..._resolvedSiteBlockSettings(),
+				[activeTabUrl]: newSettings,
+			});
+		}
+	};
 
 	return (
 		<details
@@ -186,7 +244,229 @@ function BlockSettingSummary(props: { name: string }) {
 			name={props.name}
 		>
 			<summary class="collapse-title font-semibold">Block Settings</summary>
-			<div class="collapse-content text-sm"></div>
+			<div class="collapse-content text-sm">
+				<BaseButton
+					class="btn-primary btn-sm"
+					onClick={handleAddNewBlockSetting}
+				>
+					🞦 Add Block Rule
+				</BaseButton>
+
+				<ul class="list rounded-box bg-base-100 shadow-md">
+					<Index each={blockSettings()}>
+						{(blockSetting, idx) => {
+							const [tempBlockSettings, setTempBlockSettings] = createStore(
+								blockSetting(),
+							);
+
+							return (
+								<div class="overflow-auto contain-inline-size">
+									<li class="w-max list-row place-items-center gap-4">
+										<label class="flex flex-col gap-2">
+											<span class="label text-sm">Enabled?</span>
+
+											<input
+												checked={tempBlockSettings.enabled}
+												class="toggle toggle-primary toggle-sm"
+												onInput={(e) =>
+													setTempBlockSettings("enabled", e.target.checked)
+												}
+												type="checkbox"
+											/>
+										</label>
+
+										<label class="select select-sm">
+											<span class="label"> Type</span>
+											<select
+												onInput={(e) =>
+													//@ts-expect-error eh, this is fine
+													setTempBlockSettings("type", e.target.value)
+												}
+											>
+												<For each={blockTypes}>
+													{(val) => (
+														<option
+															selected={tempBlockSettings.type === val}
+															value={val}
+														>
+															{capitalizeString(val)}
+														</option>
+													)}
+												</For>
+											</select>
+										</label>
+
+										<Switch>
+											{/*Extensions*/}
+											<Match
+												when={
+													tempBlockSettings.type === "ext" && tempBlockSettings
+												}
+											>
+												{(blockSetting) => (
+													<label class="input input-sm">
+														<span class="label">Extension</span>
+
+														<input
+															onInput={(e) =>
+																setTempBlockSettings({
+																	...blockSetting(),
+																	extension: e.target.value,
+																})
+															}
+															type="text"
+															value={blockSetting().extension}
+														/>
+													</label>
+												)}
+											</Match>
+
+											{/*Mimetype*/}
+											<Match
+												when={
+													tempBlockSettings.type === "mime" && tempBlockSettings
+												}
+											>
+												{(blockSetting) => (
+													<label class="input input-sm">
+														<span class="label">Mimetype</span>
+
+														<input
+															onInput={(e) =>
+																setTempBlockSettings({
+																	...blockSetting(),
+																	mimePattern: e.target.value,
+																})
+															}
+															type="text"
+															value={blockSetting().mimePattern}
+														/>
+													</label>
+												)}
+											</Match>
+
+											{/*Url*/}
+											<Match
+												when={
+													tempBlockSettings.type === "url" && tempBlockSettings
+												}
+											>
+												{(blockSetting) => (
+													<label class="input input-sm">
+														<span class="label">Url Pattern</span>
+
+														<input
+															onInput={(e) =>
+																setTempBlockSettings({
+																	...blockSetting(),
+																	urlPattern: e.target.value,
+																})
+															}
+															type="text"
+															value={blockSetting().urlPattern}
+														/>
+													</label>
+												)}
+											</Match>
+
+											{/*File Type*/}
+											<Match
+												when={
+													tempBlockSettings.type === "type" && tempBlockSettings
+												}
+											>
+												{(blockSetting) => {
+													const FILE_TYPE_SCHEMA =
+														BLOCK_SETTING_SCHEMA.options[0].entries.fileType;
+													type FILE_TYPE_SCHEMA = v.InferOutput<
+														typeof FILE_TYPE_SCHEMA
+													>;
+
+													const FILE_TYPES = [
+														"audio",
+														"font",
+														"image",
+														"video",
+													] as const satisfies FILE_TYPE_SCHEMA[];
+
+													return (
+														<label class="select select-sm">
+															<span class="label">File Type</span>
+
+															<select
+																onInput={(e) =>
+																	setTempBlockSettings({
+																		...blockSetting(),
+																		fileType: v.parse(
+																			FILE_TYPE_SCHEMA,
+																			e.target.value,
+																		),
+																	})
+																}
+															>
+																<For each={FILE_TYPES}>
+																	{(fileType) => (
+																		<option
+																			selected={
+																				blockSetting().fileType === fileType
+																			}
+																			value={fileType}
+																		>
+																			{capitalizeString(fileType)}
+																		</option>
+																	)}
+																</For>
+															</select>
+														</label>
+													);
+												}}
+											</Match>
+										</Switch>
+
+										<label class="input input-sm">
+											<span class="label">Min Size (KB)</span>
+
+											<input
+												class="w-12"
+												min={0}
+												onInput={(e) =>
+													setTempBlockSettings(
+														"minSize",
+														Number(e.target.value),
+													)
+												}
+												type="number"
+												value={tempBlockSettings.minSize}
+											/>
+										</label>
+
+										{/* Other buttons */}
+										<div class="flex flex-wrap items-center justify-center gap-2">
+											<BaseButton
+												aria-label="Save Changes"
+												class="btn-primary btn-circle btn-xs"
+												onClick={() =>
+													handleUpdateBlockSetting(idx, tempBlockSettings)
+												}
+											>
+												💾
+											</BaseButton>
+
+											<BaseButton
+												aria-label="Delete Block Settings"
+												class="btn-error btn-circle btn-xs"
+												onClick={() => handleRemoveBlockSetting(idx)}
+											>
+												🞭
+											</BaseButton>
+										</div>
+									</li>
+								</div>
+							);
+						}}
+					</Index>
+				</ul>
+			</div>
 		</details>
 	);
 }
@@ -317,7 +597,10 @@ function ProxySettingSummary(props: { name: string }) {
 						/>
 					</fieldset>
 
-					<BaseButton class="btn-primary place-self-center" type="submit">
+					<BaseButton
+						class="btn-primary btn-sm place-self-center"
+						type="submit"
+					>
 						Save Changes
 					</BaseButton>
 				</form>
