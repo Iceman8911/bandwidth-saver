d7677b3db0ae64083e10ed0d8e4df3c86631b21c
Iceman8911 <wuchiJSS3@gmail.com>
2025-11-26 17:03:18 +0100

    refactor: use constants over enum

--- packages/extension/shared/constants.ts
+++ packages/extension/shared/constants.ts
@@ -8,40 +8,41 @@ export const DUMMY_TAB_URL = v.parse(UrlSchema, "foo://bar");
 export const ACTIVE_TAB_URL = async () =>
 	(await getActiveTabOrigin()) ?? DUMMY_TAB_URL;
 
-export enum MessageType {
-	/** Check if the url returns a valid response code */
-	VALIDATE_URL = "0",
-
+export const MessageType = {
 	/** Sends the bandwidth used from content scripts to the background */
-	MONITOR_BANDWIDTH_WITH_PERFORMANCE_API = "1",
+	MONITOR_BANDWIDTH_WITH_PERFORMANCE_API: "1",
+	/** Check if the url returns a valid response code */
+	VALIDATE_URL: "0",
 
 	// MONITOR_BANDWIDTH_WITH_WEB_REQUEST = "2",
-}
+} as const;
+export type MessageType = (typeof MessageType)[keyof typeof MessageType];
 
-export enum CompressionMode {
+export const CompressionMode = {
+	/** Patches globals to redirect most image requests.
+	 *
+	 * Rather brittle but it's 100% client side and MV3 compatible.
+	 */
+	MONKEY_PATCH: 1,
+
+	/** Uses `declartiveNetRequest` to redirect requests to a remote proxy running the code from the `proxy` package, which will handle the rest.
+	 *
+	 */
+	PROXY: 2,
 	/** Uses `declartiveNetRequest` to redirect requests to a single compressor endpoint.
 	 *
 	 * 	Failed redirects on `img` elements fall back to the original url
 	 */
-	SIMPLE,
+	SIMPLE: 3,
 
 	/** Only avaible for MV2.
 	 *
 	 * Uses `webRequestBlocking` to modify and redirect requests to any working endpoint.
 	 */
-	WEB_REQUEST,
-
-	/** Patches globals to redirect most image requests.
-	 *
-	 * Rather brittle but it's 100% client side and MV3 compatible.
-	 */
-	MONKEY_PATCH,
-
-	/** Uses `declartiveNetRequest` to redirect requests to a remote proxy running the code from the `proxy` package, which will handle the rest.
-	 *
-	 */
-	PROXY,
-}
+	WEB_REQUEST: 4,
+} as const;
+export type CompressionMode =
+	(typeof CompressionMode)[keyof typeof CompressionMode];
 
 export const StorageKey = {
 	SCHEMA_VERSION: "sync:schemaVersion",
@@ -70,6 +71,12 @@ export const StorageKey = {
 	/** Proxy settings scoped to a site */
 	SETTINGS_SITE_SCOPE_PROXY: "sync:siteScopeProxy",
 
+	/** Top-level scoped site data */
+	SITE_SCOPE_PREFIX: "local:siteScope-",
+
+	/** Top-level scoped site statistics */
+	SITE_SCOPE_STATISTICS_PREFIX: "local:siteScope-",
+
 	/** Device-specific global statistics */
 	STATISTICS: "local:statistics",
 
@@ -78,16 +85,16 @@ export const StorageKey = {
 } as const satisfies Record<string, StorageItemKey>;
 export type StorageKey = (typeof StorageKey)[keyof typeof StorageKey];
 
-export enum DeclarativeNetRequestRuleIds {
-	ADD_SAVE_DATA_HEADER = 1,
-
-	REDIRECT_TO_SIMPLE_COMPRESSION_ENDPOINT,
-
-	/** Disables related rules since `declartiveNetRequest` will not be used */
-	ENABLE_WEB_REQUEST,
+export const DeclarativeNetRequestRuleIds = {
+	ADD_SAVE_DATA_HEADER: 1,
 
 	/** Disables related rules since code for this runs on the client-page itself */
-	ENABLE_MONKEY_PATCH,
+	ENABLE_MONKEY_PATCH: 2,
 
-	REDIRECT_TO_REMOTE_POXY,
-}
+	/** Disables related rules since `declartiveNetRequest` will not be used */
+	ENABLE_WEB_REQUEST: 3,
+
+	REDIRECT_TO_REMOTE_PROXY: 4,
+
+	REDIRECT_TO_SIMPLE_COMPRESSION_ENDPOINT: 5,
+};
--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -1,10 +1,11 @@
-export enum ImageCompressorEndpoint {
-	WSRV_NL = "https://wsrv.nl",
+export const ImageCompressorEndpoint = {
+	/** Not working :( */
+	ALPACA_CDN: "https://spitting.alpacacdn.com",
 
 	/** Has watermarks */
-	FLY_IMG_IO = "https://demo.flyimg.io",
-
-	/** Not working :( */
-	ALPACA_CDN = "https://spitting.alpacacdn.com",
-}
+	FLY_IMG_IO: "https://demo.flyimg.io",
+	WSRV_NL: "https://wsrv.nl",
+} as const;
+export type ImageCompressorEndpoint =
+	(typeof ImageCompressorEndpoint)[keyof typeof ImageCompressorEndpoint];
 // TODO: Add bandwidth hero service and maybe a custom selfhost one
