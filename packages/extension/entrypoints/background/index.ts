e07b44930551b47a9f0791a8df1e9a9e155af740
Iceman8911 <wuchiJSS3@gmail.com>
2025-11-29 16:42:23 +0100

    refactor: move `save-data.ts` up one directory

--- packages/extension/entrypoints/background/index.ts
+++ packages/extension/entrypoints/background/index.ts
@@ -1,4 +1,4 @@
-import { saveDataToggleWatcher } from "./compression/save-data";
+import { saveDataToggleWatcher } from "./save-data";
 import { processMonitoredBandwidthData } from "./statistics/bandwidth-calculation";
 import { monitorBandwidthUsageViaBackground } from "./statistics/bandwidth-monitoring";
 
