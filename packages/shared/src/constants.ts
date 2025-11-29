c70143fb90037971a386bfd8ea6717c0e1eacf73
Iceman8911 <wuchiJSS3@gmail.com>
2025-11-29 21:56:56 +0100

    refactor: use enum

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -1,11 +1,11 @@
-export const ImageCompressorEndpoint = {
+export enum ImageCompressorEndpoint {
 	/** Not working :( */
-	ALPACA_CDN: "https://spitting.alpacacdn.com",
+	ALPACA_CDN = "https://spitting.alpacacdn.com",
 
 	/** Has watermarks */
-	FLY_IMG_IO: "https://demo.flyimg.io",
-	WSRV_NL: "https://wsrv.nl",
-} as const;
-export type ImageCompressorEndpoint =
-	(typeof ImageCompressorEndpoint)[keyof typeof ImageCompressorEndpoint];
+	FLY_IMG_IO = "https://demo.flyimg.io",
+	WSRV_NL = "https://wsrv.nl",
+
+	DEFAULT = WSRV_NL,
+}
 // TODO: Add bandwidth hero service and maybe a custom selfhost one
