372bbee77ceaac8d9f7d463363ee079ec1e03198
Iceman8911 <wuchiJSS3@gmail.com>
2025-12-09 11:21:11 +0100

    feat: basic work on the proxy compressor
    now i need to add `sharp` image processing

--- packages/proxy/src/index.ts
+++ packages/proxy/src/index.ts
@@ -2,31 +2,50 @@ import {
 	getCompressedImageUrlWithFallback,
 	getProxyEnv,
 	ImageCompressionPayloadSchema,
+	REDIRECTED_SEARCH_PARAM_FLAG,
 	ServerAPIEndpoint,
 } from "@bandwidth-saver/shared";
 import { Elysia } from "elysia";
 
 const env = getProxyEnv();
 
+const [REDIRECTED_SEARCH_PARAM_KEY = "", REDIRECTED_SEARCH_PARAM_VALUE = ""] =
+	REDIRECTED_SEARCH_PARAM_FLAG.split("=");
+
 const app = new Elysia()
 	.get(
 		`/${ServerAPIEndpoint.COMPRESS_IMAGE}`,
 		// TODO: Maybe add custom compression using `imgproxy` or `sharp`
-		async ({ query, redirect, status }) => {
-			try {
-				const redirectedUrl = await getCompressedImageUrlWithFallback(query);
+		async ({ query, redirect }) => {
+			const redirectedUrl = await getCompressedImageUrlWithFallback(query);
 
-				return redirect(redirectedUrl);
-			} catch {
-				return status(400, {
-					message: "Failed to compress image",
-				});
+			if (redirectedUrl !== query.url) {
+				const redirectedUrlObject = new URL(redirectedUrl);
+
+				redirectedUrlObject.searchParams.append(
+					REDIRECTED_SEARCH_PARAM_KEY,
+					REDIRECTED_SEARCH_PARAM_VALUE,
+				);
+
+				return redirect(String(redirectedUrlObject));
+			} else {
+				// custom compression
 			}
 		},
 		{
 			query: ImageCompressionPayloadSchema,
 		},
 	)
-	.listen(env.VITE_SERVER_PORT);
+	.listen(
+		{
+			hostname: env.VITE_SERVER_HOST,
+			port: env.VITE_SERVER_PORT,
+		},
+		(server) => {
+			console.log(
+				`Elysia server running at http://${server.hostname}:${server.port}`,
+			);
+		},
+	);
 
 export type ElysiaApp = typeof app;
