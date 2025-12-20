2a9f046c703cc12074e404d2bfc7f040598e1db8
Iceman8911 <wuchiJSS3@gmail.com>
2025-12-20 12:54:51 +0100

    refactor: use kobalte button component

--- packages/extension/components/button.tsx
+++ packages/extension/components/button.tsx
@@ -1,13 +1,14 @@
+import { Button } from "@kobalte/core/button";
 import type { ComponentProps } from "solid-js";
 
 export function BaseButton(props: ComponentProps<"button">) {
 	return (
-		<button
+		<Button
 			{...props}
 			class={`btn ${props.class || ""}`}
 			type={props.type ?? "button"}
 		>
 			{props.children}
-		</button>
+		</Button>
 	);
 }
