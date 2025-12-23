f218fe419f0fda4fb915e8cd8bf239c2ef503d6a
Iceman8911 <wuchiJSS3@gmail.com>
2025-12-23 14:53:56 +0100

    wip: add base structure for option page nav bar

---
+++ packages/extension/entrypoints/options/components/nav-bar.tsx
@@ -0,0 +1,68 @@
+import { NavigationMenu } from "@kobalte/core/navigation-menu";
+import { A } from "@solidjs/router";
+import type { JSXElement } from "solid-js";
+import Logo from "@/assets/icon.png";
+
+function ExtensionLogoAndName() {
+	return (
+		<div class="flex items-center justify-center gap-4">
+			<div class="avatar">
+				<div class="size-10 rounded">
+					<img alt="Bandwidth Saver and Monitor Logo" src={Logo} />
+				</div>
+			</div>
+
+			<h1 class="font-bold text-base">Bandwidth Saver And Monitor</h1>
+		</div>
+	);
+}
+
+type NavigationBarLinkProps = {
+	href: `/${string}`;
+	children: JSXElement;
+	isSelected?: boolean;
+};
+
+function NavigationBarLink(props: NavigationBarLinkProps) {
+	return (
+		<NavigationMenu.Trigger
+			activeClass="menu-active"
+			as={A}
+			end={true}
+			href={props.href}
+		>
+			{props.children}
+		</NavigationMenu.Trigger>
+	);
+}
+
+function NavigationBarLinks() {
+	return (
+		<NavigationMenu
+			class="navbar menu menu-horizontal *:text-base"
+			orientation="horizontal"
+		>
+			<NavigationMenu.Menu>
+				<NavigationBarLink href="/">Overview</NavigationBarLink>
+
+				<NavigationBarLink href="/compression">Compression</NavigationBarLink>
+
+				<NavigationBarLink href="/blocking">Blocking</NavigationBarLink>
+
+				<NavigationBarLink href="/proxy">Proxy</NavigationBarLink>
+
+				<NavigationBarLink href="/about">About</NavigationBarLink>
+			</NavigationMenu.Menu>
+		</NavigationMenu>
+	);
+}
+
+export default function OptionsPageNavigationBar() {
+	return (
+		<div class="flex h-16 items-center justify-around bg-base-300 p-4">
+			<ExtensionLogoAndName />
+
+			<NavigationBarLinks />
+		</div>
+	);
+}
--- packages/extension/entrypoints/options/layout.tsx
+++ packages/extension/entrypoints/options/layout.tsx
@@ -1,11 +1,12 @@
 import type { JSXElement } from "solid-js";
+import OptionsPageNavigationBar from "./components/nav-bar";
 
 type OptionsPageLayoutProps = { children?: JSXElement };
 
 export default function OptionsPageLayout(props: OptionsPageLayoutProps) {
 	return (
 		<div class="h-screen w-screen">
-			{/* Top level navigation bar here */}
+			<OptionsPageNavigationBar />
 
 			{props.children}
 		</div>
