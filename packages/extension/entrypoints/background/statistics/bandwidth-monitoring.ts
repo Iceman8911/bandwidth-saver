d77281ad1e5b692687de39ac4a45603b452588fd
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-14 15:47:03 +0100

    wip(extension): use batch queue in webRequest statisitcs collector

--- packages/extension/entrypoints/background/statistics/bandwidth-monitoring.ts
+++ packages/extension/entrypoints/background/statistics/bandwidth-monitoring.ts
@@ -1,7 +1,23 @@
+import type { UrlSchema } from "@bandwidth-saver/shared";
 import type { SingleAssetStatisticsSchema } from "@/models/storage";
 import { DUMMY_TAB_URL } from "@/shared/constants";
 import { detectAssetTypeFromUrl } from "@/utils/url";
+import BatchQueue from "../../../../shared/src/utils/batch";
 import { cacheBandwidthDataFromWebRequest } from "./bandwidth-calculation";
+
+type RelevantPropsFromOnCompletedEventPayload = {
+	responseHeaders: Browser.webRequest.HttpHeader[];
+	type: Browser.webRequest.OnCompletedDetails["type"];
+	url: UrlSchema;
+	fromCache: boolean;
+	initiator: UrlSchema;
+};
+
+const pendingWebRequestPayloadBatchQueue =
+	new BatchQueue<RelevantPropsFromOnCompletedEventPayload>({
+		batchSize: 25,
+		intervalMs: 750,
+	});
 
 function detectAssetTypeFromContentTypeOrUrl(
 	url: string | URL,
@@ -33,63 +49,89 @@ function detectAssetTypeFromContentTypeOrUrl(
 	return detectAssetTypeFromUrl(parsedUrl);
 }
 
+function webRequestOnCompletedListener({
+	fromCache,
+	initiator,
+	responseHeaders,
+	type,
+	url,
+}: RelevantPropsFromOnCompletedEventPayload) {
+	// No need to bother ourselves if the asset is cached
+	if (fromCache) return;
+
+	const parsedUrl = new URL(url);
+
+	let contentLength = 0;
+	let contentType = "other";
+
+	for (const header of responseHeaders) {
+		const headerName = header.name.toLowerCase();
+		const headerValue = header.value;
+
+		if (headerName === "content-length") {
+			contentLength = Number(headerValue);
+		} else if (headerName === "content-type") {
+			contentType = (headerValue ?? contentType).toLowerCase();
+		}
+	}
+
+	let assetType: keyof SingleAssetStatisticsSchema = "other";
+
+	switch (type) {
+		case "stylesheet":
+			assetType = "style";
+			break;
+		case "script":
+			assetType = "script";
+			break;
+		case "image":
+			assetType = "image";
+			break;
+		case "font":
+			assetType = "font";
+			break;
+		default:
+			assetType = detectAssetTypeFromContentTypeOrUrl(parsedUrl, contentType);
+			break;
+	}
+
+	cacheBandwidthDataFromWebRequest({
+		//@ts-expect-error a stringified URL object will always be a valid url
+		assetUrl: `${parsedUrl}`,
+		bytes: contentLength,
+		hostOrigin: getUrlSchemaOrigin(initiator),
+		type: assetType,
+	});
+
+	return undefined;
+}
+
+pendingWebRequestPayloadBatchQueue.addCallbacks((payloads) => {
+	for (const payload of payloads) {
+		webRequestOnCompletedListener(payload);
+	}
+});
+
 export function monitorBandwidthUsageViaBackground() {
 	browser.webRequest.onCompleted.addListener(
-		({ responseHeaders = [], type, url, fromCache, initiator }) => {
-			// No need to bother ourselves if the asset is cached
-			if (fromCache) return;
+		({
+			fromCache,
+			initiator = DUMMY_TAB_URL,
+			responseHeaders = [],
+			type,
+			url,
+		}) => {
+			pendingWebRequestPayloadBatchQueue.enqueue({
+				fromCache,
 
-			const parsedUrl = new URL(url);
+				//@ts-expect-error the initiator will always be a valid url
+				initiator,
+				responseHeaders,
+				type,
 
-			let contentLength = 0;
-			let contentType = "other";
-
-			for (const header of responseHeaders) {
-				const headerName = header.name.toLowerCase();
-				const headerValue = header.value;
-
-				if (headerName === "content-length") {
-					contentLength = Number(headerValue);
-				} else if (headerName === "content-type") {
-					contentType = (headerValue ?? contentType).toLowerCase();
-				}
-			}
-
-			let assetType: keyof SingleAssetStatisticsSchema = "other";
-
-			switch (type) {
-				case "stylesheet":
-					assetType = "style";
-					break;
-				case "script":
-					assetType = "script";
-					break;
-				case "image":
-					assetType = "image";
-					break;
-				case "font":
-					assetType = "font";
-					break;
-				default:
-					assetType = detectAssetTypeFromContentTypeOrUrl(
-						parsedUrl,
-						contentType,
-					);
-					break;
-			}
-
-			cacheBandwidthDataFromWebRequest({
-				//@ts-expect-error a stringified URL object will always be a valid url
-				assetUrl: `${parsedUrl}`,
-				bytes: contentLength,
-				hostOrigin: getUrlSchemaOrigin(
-					//@ts-expect-error the initiator will always be a valid url
-					initiator ?? DUMMY_TAB_URL,
-				),
-				type: assetType,
+				//@ts-expect-error the initiator will always be a valid url
+				url,
 			});
-
-			return undefined;
 		},
 		{ urls: ["<all_urls>"] },
 		["responseHeaders"],
