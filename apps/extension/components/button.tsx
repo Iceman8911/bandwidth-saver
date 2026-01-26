5716fa1714876fe47f0e2afeef095c23961f20c0
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-26 20:31:14 +0100

    refactor(extension): remove useless kobalte component
    no reason not to use the native button

--- apps/extension/components/button.tsx
+++ apps/extension/components/button.tsx
@@ -1,14 +1,13 @@
-import { Button } from "@kobalte/core/button";
 import type { ComponentProps } from "solid-js";
 
 export function BaseButton(props: ComponentProps<"button">) {
 	return (
-		<Button
+		<button
 			{...props}
 			class={`btn ${props.class || ""}`}
 			type={props.type ?? "button"}
 		>
 			{props.children}
-		</Button>
+		</button>
 	);
 }
