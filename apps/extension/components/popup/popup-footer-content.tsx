3b7cea13ac340fd5a084f5cb553ab998adc0ddf3
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-01 11:01:42 +0100

    wip(extension, popup): use kobalte dropdown + daisyui menu

--- apps/extension/components/popup/popup-footer-content.tsx
+++ apps/extension/components/popup/popup-footer-content.tsx
@@ -1,17 +1,64 @@
+import { DropdownMenu } from "@kobalte/core/dropdown-menu";
+import { ChevronDown, Download, Settings, Upload } from "lucide-solid";
+import { createSignal } from "solid-js";
 import { browser } from "wxt/browser";
 import { BaseButton } from "../button";
 
+function OptionDropdownContent() {
+	return (
+		<DropdownMenu.Portal>
+			<DropdownMenu.Content
+				as="ul"
+				class="menu rounded-box border border-base-300 bg-base-100 p-2 shadow-sm"
+			>
+				<DropdownMenu.Item as="li">
+					<button type="button">
+						<Download />
+						Import Settings
+					</button>
+				</DropdownMenu.Item>
+				<DropdownMenu.Item as="li">
+					<button type="button">
+						<Upload />
+						Export Settings
+					</button>
+				</DropdownMenu.Item>
+				<DropdownMenu.Item
+					as="li"
+					onClick={() => void browser.runtime.openOptionsPage()}
+				>
+					<button type="button">
+						<Settings />
+						Options Page
+					</button>
+				</DropdownMenu.Item>
+				<DropdownMenu.Arrow />
+			</DropdownMenu.Content>
+		</DropdownMenu.Portal>
+	);
+}
+
 export default function PopupFooterContent() {
+	const [isDropdownOpen, setIsDropdownOpen] = createSignal(false);
+
 	return (
 		<footer class="flex items-center justify-between rounded-box bg-base-200 p-4 text-sm">
 			<span>Made with love :3</span>
 
-			<BaseButton
-				class="btn-soft"
-				onClick={() => void browser.runtime.openOptionsPage()}
-			>
-				Options
-			</BaseButton>
+			<DropdownMenu onOpenChange={setIsDropdownOpen} open={isDropdownOpen()}>
+				<DropdownMenu.Trigger>
+					<BaseButton class="btn-soft">
+						More
+						<DropdownMenu.Icon>
+							<ChevronDown
+								class={`transition-transform ${isDropdownOpen() ? "rotate-180" : ""}`}
+							/>
+						</DropdownMenu.Icon>
+					</BaseButton>
+				</DropdownMenu.Trigger>
+
+				<OptionDropdownContent />
+			</DropdownMenu>
 		</footer>
 	);
 }
