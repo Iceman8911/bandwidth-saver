5b0d44255df0bdbcc21200e596e192e90b0357e9
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-01 21:31:10 +0100

    feat(extension, popup): add functionality to the import and export buttons

--- apps/extension/components/popup/popup-footer-content.tsx
+++ apps/extension/components/popup/popup-footer-content.tsx
@@ -1,10 +1,62 @@
 import { DropdownMenu } from "@kobalte/core/dropdown-menu";
 import { ChevronDown, Download, Settings, Upload } from "lucide-solid";
-import { createSignal } from "solid-js";
+import { createResource, createSignal, onCleanup } from "solid-js";
+import { useFileDialog, useObjectUrl } from "solidjs-use";
 import { browser } from "wxt/browser";
+import {
+	exportExtensionSettingsAsString,
+	importExtensionSettingsFromString,
+} from "@/utils/import-and-export";
 import { BaseButton } from "../button";
 
+const downloads = browser.downloads;
+
+const MIME_TYPE = "application/json";
+const FILE_NAME = "bandwidth-saver-settings.json";
+
 function OptionDropdownContent() {
+	const { onChange: onFileDialogChange, open: openFileDialog } = useFileDialog({
+		accept: MIME_TYPE,
+		multiple: false,
+	});
+
+	const { off: fileDialogCleanup } = onFileDialogChange(async (fileList) => {
+		const possibleFileToImport = fileList?.item(0);
+
+		if (possibleFileToImport) {
+			await importExtensionSettingsFromString(
+				await possibleFileToImport.text(),
+			);
+		}
+	});
+
+	onCleanup(() => {
+		fileDialogCleanup();
+	});
+
+	const [
+		exportableSettingsJsonFile,
+		{ refetch: refetchExportableSettingsJsonFile },
+	] = createResource(async () => {
+		const str = await exportExtensionSettingsAsString();
+
+		return new File([str], FILE_NAME, { type: MIME_TYPE });
+	});
+
+	const exportableSettingsJsonFileUrl = useObjectUrl(
+		exportableSettingsJsonFile,
+	);
+
+	const handleExportSettingsToUserDevice = async () => {
+		await refetchExportableSettingsJsonFile();
+
+		const url = exportableSettingsJsonFileUrl();
+
+		if (url) {
+			await downloads.download({ filename: FILE_NAME, url });
+		}
+	};
+
 	return (
 		<DropdownMenu.Portal>
 			<DropdownMenu.Content
@@ -12,13 +64,13 @@ function OptionDropdownContent() {
 				class="menu rounded-box border border-base-300 bg-base-100 p-2 shadow-sm"
 			>
 				<DropdownMenu.Item as="li">
-					<button type="button">
+					<button onClick={() => openFileDialog()} type="button">
 						<Download />
 						Import Settings
 					</button>
 				</DropdownMenu.Item>
 				<DropdownMenu.Item as="li">
-					<button type="button">
+					<button onClick={handleExportSettingsToUserDevice} type="button">
 						<Upload />
 						Export Settings
 					</button>
