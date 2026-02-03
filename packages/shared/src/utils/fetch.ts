3e3f83f5e4e52346b762213753459f0a37391214
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-03 19:12:20 +0100

    feat(shared): add spoofing headers

--- packages/shared/src/utils/fetch.ts
+++ packages/shared/src/utils/fetch.ts
@@ -95,3 +95,17 @@ export function getLikelyImageUrlMimeType(
 export function getFetchTimeoutSignal(timeoutInMs = 2500): AbortSignal {
 	return AbortSignal.timeout(timeoutInMs);
 }
+
+export const SPOOFING_FETCH_HEADERS = {
+	Accept:
+		"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+	"Accept-Encoding": "gzip, deflate, br",
+	"Accept-Language": "en-US,en;q=0.9",
+	Connection: "keep-alive",
+	"Sec-Fetch-Dest": "document",
+	"Sec-Fetch-Mode": "navigate",
+	"Sec-Fetch-Site": "none",
+	"Upgrade-Insecure-Requests": "1",
+	"User-Agent":
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+} as const;
