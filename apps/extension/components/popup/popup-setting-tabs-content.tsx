c566f9e03f0993f0e1a2bd5620e2b1270e19aaa1
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-08 07:56:49 +0100

    refactor(extension, popup): use kobalte tabs over pure daisy ui ones
    less boilerplate and this'll look better with overflow

--- apps/extension/components/popup/popup-setting-tabs-content.tsx
+++ apps/extension/components/popup/popup-setting-tabs-content.tsx
@@ -1,91 +1,89 @@
-import { getRandomUUID } from "@bandwidth-saver/shared";
+import { Tabs } from "@kobalte/core";
 import { FileArchive, Network, Settings } from "lucide-solid";
-import { createSelector, createSignal, type JSXElement } from "solid-js";
+import { For, type JSXElement } from "solid-js";
+import { Dynamic } from "solid-js/web";
 import PopupCompressionSettings from "./popup-compression-settings";
 import PopupOtherSettings from "./popup-other-settings";
 import PopupProxySettings from "./popup-proxy-settings";
 
-type TabWrapperProps = {
-	name: string;
-	header: JSXElement;
-	children: JSXElement;
-	isSelected: boolean;
-	setIsSelected: () => void;
-};
+interface TabsComponentProps<TTabValue extends string> {
+	default: TTabValue;
+	tabs: ReadonlyArray<{
+		val: TTabValue;
+		header: () => JSXElement;
+		content: () => JSXElement;
+	}>;
+}
 
-function TabWrapper(props: TabWrapperProps) {
+function TabsComponent<TTabValue extends string>(
+	props: TabsComponentProps<TTabValue>,
+) {
 	return (
-		<>
-			<label class="tab">
-				<input
-					checked={props.isSelected}
-					name={props.name}
-					onClick={props.setIsSelected}
-					type="radio"
-				/>
-
-				<div class="flex gap-2">{props.header}</div>
-			</label>
-
-			<div class="tab-content overflow-auto border-base-300 bg-base-200 p-4 [scrollbar-gutter:stable]">
-				{props.children}
-			</div>
-		</>
+		<Tabs.Root
+			class="flex size-full flex-col contain-size"
+			defaultValue={props.default}
+		>
+			<Tabs.List class="relative flex w-full">
+				<For each={props.tabs}>
+					{(tab) => (
+						<Tabs.Trigger class="btn btn-ghost rounded-none" value={tab.val}>
+							<Dynamic class="flex gap-2" component={tab.header} />
+						</Tabs.Trigger>
+					)}
+				</For>
+				<Tabs.Indicator class="absolute -bottom-1 h-0.5 bg-primary transition-all" />
+			</Tabs.List>
+			<For each={props.tabs}>
+				{(tab) => (
+					<Tabs.Content
+						class="h-full overflow-auto rounded-b-box bg-base-200 p-4 [scrollbar-gutter:stable]"
+						value={tab.val}
+					>
+						<Dynamic component={tab.content} />
+					</Tabs.Content>
+				)}
+			</For>
+		</Tabs.Root>
 	);
 }
 
 type SelectedTab = "compress" | "proxy" | "other";
 
+const DEFAULT_TAB: SelectedTab = "compress";
+
 export default function PopupSettingsTabsContent() {
-	const [selectedTab, setSelectedTab] = createSignal<SelectedTab>("compress");
-
-	const isTabSelected = createSelector(selectedTab);
-
-	const tabGroupName = getRandomUUID();
-
 	return (
-		<div class="tabs tabs-lift">
-			<TabWrapper
-				header={
-					<>
-						<FileArchive />
-						Compression
-					</>
-				}
-				isSelected={isTabSelected("compress")}
-				name={tabGroupName}
-				setIsSelected={() => setSelectedTab("compress")}
-			>
-				<PopupCompressionSettings />
-			</TabWrapper>
-
-			<TabWrapper
-				header={
-					<>
-						<Network />
-						Proxy
-					</>
-				}
-				isSelected={isTabSelected("proxy")}
-				name={tabGroupName}
-				setIsSelected={() => setSelectedTab("proxy")}
-			>
-				<PopupProxySettings />
-			</TabWrapper>
-
-			<TabWrapper
-				header={
-					<>
-						<Settings />
-						Other
-					</>
-				}
-				isSelected={isTabSelected("other")}
-				name={tabGroupName}
-				setIsSelected={() => setSelectedTab("other")}
-			>
-				<PopupOtherSettings />
-			</TabWrapper>
-		</div>
+		<TabsComponent
+			default={DEFAULT_TAB}
+			tabs={[
+				{
+					content: PopupCompressionSettings,
+					header: () => (
+						<>
+							<FileArchive /> Compression
+						</>
+					),
+					val: "compress",
+				},
+				{
+					content: PopupProxySettings,
+					header: () => (
+						<>
+							<Network /> Proxy
+						</>
+					),
+					val: "proxy",
+				},
+				{
+					content: PopupOtherSettings,
+					header: () => (
+						<>
+							<Settings /> Other
+						</>
+					),
+					val: "other",
+				},
+			]}
+		/>
 	);
 }
