1137eba4342df18baf028536a37455fc418530bc
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-09 00:27:32 +0100

    refactor(extension) comment out ineffective alaram
    i don't really do anything with the updated value. i even forgot this
    existed >~<

--- apps/extension/entrypoints/background/dnr-rules/whilisted-domains.ts
+++ apps/extension/entrypoints/background/dnr-rules/whilisted-domains.ts
@@ -7,42 +7,42 @@ import { ALARM_NAME, UPDATE_INTERVAL_IN_MINUTES } from "@/shared/constants";
 const REMOTE_COMPRESSION_WHITELISTED_DOMAIN_URL =
 	"https://raw.githubusercontent.com/iceman8911/bandwidth-saver/main/packages/extension/data/compression-whilelisted-domains.json";
 
-let whitelistedDomains: ReadonlyArray<string> =
+const whitelistedDomains: ReadonlyArray<string> =
 	DEFAULT_COMPRESSION_WHITELISTED_DOMAIN_JSON.domains;
 
-async function getUpToDateWhitelistedDomains(): Promise<ReadonlyArray<string>> {
-	try {
-		const possibleUpdatedJson = v.parse(
-			CompressionWhitelistedDomainSchema,
-			await (await fetch(REMOTE_COMPRESSION_WHITELISTED_DOMAIN_URL)).json(),
-		);
+// async function getUpToDateWhitelistedDomains(): Promise<ReadonlyArray<string>> {
+// 	try {
+// 		const possibleUpdatedJson = v.parse(
+// 			CompressionWhitelistedDomainSchema,
+// 			await (await fetch(REMOTE_COMPRESSION_WHITELISTED_DOMAIN_URL)).json(),
+// 		);
 
-		if (
-			possibleUpdatedJson.version >
-			DEFAULT_COMPRESSION_WHITELISTED_DOMAIN_JSON.version
-		)
-			return possibleUpdatedJson.domains;
+// 		if (
+// 			possibleUpdatedJson.version >
+// 			DEFAULT_COMPRESSION_WHITELISTED_DOMAIN_JSON.version
+// 		)
+// 			return possibleUpdatedJson.domains;
 
-		return whitelistedDomains;
-	} catch {
-		return whitelistedDomains;
-	}
-}
+// 		return whitelistedDomains;
+// 	} catch {
+// 		return whitelistedDomains;
+// 	}
+// }
 
-const alarms = browser.alarms;
+// const alarms = browser.alarms;
 
-alarms.create(ALARM_NAME.WHITELISTED_DOMAIN_SYNC, {
-	periodInMinutes: UPDATE_INTERVAL_IN_MINUTES,
-});
+// alarms.create(ALARM_NAME.WHITELISTED_DOMAIN_SYNC, {
+// 	periodInMinutes: UPDATE_INTERVAL_IN_MINUTES,
+// });
 
-async function whitelistedDomainsSyncListener(alarm: Browser.alarms.Alarm) {
-	if (alarm.name !== ALARM_NAME.WHITELISTED_DOMAIN_SYNC) return;
+// async function whitelistedDomainsSyncListener(alarm: Browser.alarms.Alarm) {
+// 	if (alarm.name !== ALARM_NAME.WHITELISTED_DOMAIN_SYNC) return;
 
-	whitelistedDomains = await getUpToDateWhitelistedDomains();
-}
+// 	whitelistedDomains = await getUpToDateWhitelistedDomains();
+// }
 
-alarms.onAlarm.removeListener(whitelistedDomainsSyncListener);
-alarms.onAlarm.addListener(whitelistedDomainsSyncListener);
+// alarms.onAlarm.removeListener(whitelistedDomainsSyncListener);
+// alarms.onAlarm.addListener(whitelistedDomainsSyncListener);
 
 export function getWhitelistedDomains(): ReadonlyArray<string> {
 	return whitelistedDomains;
